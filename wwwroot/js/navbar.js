f1ef6ad22997230850fc899af59843598c7d26c2
Mahaluta <adrianmahalu@gmail.com>
2020-04-29 16:58:47 +0300

    [FE - 1] - Navbar
    new navbar

---
+++ wwwroot/js/navbar.js
@@ -0,0 +1,6 @@
+﻿const toggleButton = document.getElementsByClassName('toggle-menu-button-SC')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links-SC')[0];
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+} )
